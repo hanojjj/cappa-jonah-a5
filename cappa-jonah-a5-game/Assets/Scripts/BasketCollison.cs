282aa1c439b5f48873b0b10709e065579b2f2e35
Jonah Cappa <jonahcappaofficial@gmail.com>
2023-12-11 20:45:16 -0500

    Add some collision script

--- cappa-jonah-a5-game/Assets/Scripts/BasketCollison.cs
+++ cappa-jonah-a5-game/Assets/Scripts/BasketCollison.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class BasketCollison : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.name == "Ball")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().sharedMaterial.bounciness = 1;
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionStay2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.name == "Ball")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().sharedMaterial.bounciness = 0;
+
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            collision.gameObject.GetComponent<Transform>().position = new Vector3(0, 6, 0);
+
+            collision.gameObject.GetComponent<Rigidbody2D>().sharedMaterial.bounciness = 1;
+
+        }
     }
 }
